c35334a365e9ec8d07916a4a27e1253b02420171
Jéssica Neri <jess.neri94@gmail.com>
2020-09-23 07:52:10 -0300

    DOc: incluído mais funcionalidades de pedido. Falta corrigir deletarPedido e implementar lista e marcação de estado

--- index.js
+++ index.js
@@ -196,6 +196,75 @@ const adicionarPedidos = (pedido, id, nome, quantidade, novovalor) => {
 
 }
 
+//obter informações de um pedido em particular
+const obterPedido = (id) => {
+
+    for (let i = 0; i < listaDePedidos.length; i++) {
+        if (id == listaDePedidos[i].id) {
+            return listaDePedidos[i];
+        }
+    }
+    return false;
+}
+
+//marcação de estado de pedido
+const atualizarPedido = (id, produtosAtualizados, nome, quantidade, novovalor) => {
+    const pedido = obterPedido(id);
+    console.log(pedido)
+
+    let prodEstoqueArray = [];
+    console.log(estoque);
+    estoque.forEach((prodEstoque, i) => {
+        pedido.produtos.forEach((prodPedido, i) => {
+            //se id é igual ao estoque
+            if (prodEstoque.id === prodPedido.id) {
+                if (prodEstoque.qtd > 0 && prodEstoque.qtd >= prodPedido.qtd) {
+                    prodEstoqueArray.push(prodEstoque);
+                    prodEstoque.qtd = prodEstoque.qtd - prodPedido.qtd;
+
+                }
+            }
+        });
+        atualizarProduto(id, nome, quantidade, novovalor)
+    });
+
+    if (!pedido) {
+        return null; //não encontra produto
+    } else {
+
+        if (pedido.estado.toUpperCase() === "INCOMPLETO") {
+            const pedidoAtualizado = {
+                id: pedido.id,
+                produtos: produtosAtualizados,
+                estado: pedido.estado,
+                idCliente: pedido.idCliente,
+                deletado: false,
+                valorTotal: calcularTotalDoPedido(prodEstoqueArray)
+            }
+
+            listaDePedidos.splice(pedido.index, 1, pedidoAtualizado);
+            return pedidoAtualizado;
+        } else {
+            return null;
+        }
+
+    }
+}
+
+// deletar pedido
+const deletarPedido = (id) => {
+    const pedido = obterPedido(id);
+
+    if (pedido !== false) {
+        listaDePedidos.splice(pedido, 1);
+        return true;
+    } else {
+        return "Pedido não existente!";
+    }
+}
+
+
+
 
 
 const caminhos = (ctx) => {
@@ -259,11 +328,7 @@ const caminhos = (ctx) => {
                 } else {
                     formatarErro(ctx, "Não foi possível criar o produto", 403);
                 }
-                break;
-            case "PUT":
-                break;
-            case "DELETE":
-                break;
+
             default:
                 break;
         }
@@ -272,15 +337,35 @@ const caminhos = (ctx) => {
         const id = pathQuebrado[2];
         switch (method) {
             case "GET":
+                console.log(obterPedido(id))
+                formatarSucesso(ctx, obterPedido(id), 200);
                 break;
             case "POST":
 
                 break;
             case "PUT":
+                const body = ctx.request.body;
+                const produtosAtualizados = body.produtosAtualizados;
+                const resposta = atualizarPedido(id, produtosAtualizados)
+                if (resposta) {
+                    formatarSucesso(ctx, resposta, 200);
+
+                } else {
+                    formatarErro(ctx, "Não foi possível atualizar o pedido", 403);
+                }
                 break;
             case "DELETE":
+                const deletado = deletarPedido(id);
+                if (deletado === true) {
+                    formatarSucesso(ctx, "Pedido deletado com sucesso!", 200);
+
+                } else {
+                    formatarErro(ctx, "Pedido já deletado.", 404)
+
+                }
                 break;
             default:
+                formatarErro(ctx, "Conteúdo não encontrado.", 404);
                 break;
         }
     } else {
